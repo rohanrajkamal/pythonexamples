a003523e83e1048a7653f3b58a76c4a23ce66942
rohanrajkamal <86274658+rohanrajkamal@users.noreply.github.com>
2021-07-27 18:49:40 -0700

    CToF, FToc
    Celsius to Fahrenheit, FahrenHeit to Celsius

---
+++ Conversion/Weather/farenheitToCelsius.py
@@ -0,0 +1,3 @@
+temp = float(input("Enter temperture in Fahrenheit:"))
+celsius = (temp-32)* 5/9
+print(f"{temp} in Fahrenheit is equal to {celsius} Celsius")    
--- Numbers/natural.py
+++ Numbers/natural.py
@@ -1,4 +1,4 @@
 input1=int(input("enter a number:"))
 print("prints the range of natural numbers")
 for i in range(1, input1+1):
- print("%d" % (i))
+ print("%d" % (i))``
